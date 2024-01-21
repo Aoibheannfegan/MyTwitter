2392df6c40a1124b66c2fea7e349fefae5d463b9
Aoibheann <aoibheannfegan04@gmail.com>
2024-01-20 16:33:48 -0800

    Update allowed hosts andCSRF trusted origins with railway site url

--- project4/settings.py
+++ project4/settings.py
@@ -16,7 +16,9 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
 
 
 # Application definition
